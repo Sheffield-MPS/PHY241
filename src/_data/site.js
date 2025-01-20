1fa5baa7dfecf22e3ca3da5df077833c06be92e5
Stuart Littlefair <s.littlefair@shef.ac.uk>
2025-01-20 14:08:25 +0000

    trying to get gh_pages to work

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
     name: "PHY241",
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://slittlefair.staff.shef.ac.uk/teaching/phy241'
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://slittlefair.github.io/phy241_website'
 };
