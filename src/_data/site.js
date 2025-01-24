e281c3501e9876c0105e7dc2c225b067917ad80e
Stuart Littlefair <s.littlefair@shef.ac.uk>
2025-01-24 12:29:28 +0000

    update site URL for new host org

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
     name: "PHY241",
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://stuartlittlefair.github.io/phy241_website'
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://sheffield-mps.github.io/PHY241'
 };
